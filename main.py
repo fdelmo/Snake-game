766d5dc4cd65981f8a7de64f942b9d877e04e428
Ferran <ferrandelmoral@gmail.com>
2021-12-15 18:48:17 +0100

    Load records from pickle file tested and working.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pygame
+import os
+import pickle
+from datetime import date
+from pygame.constants import CONTROLLER_BUTTON_MAX
 from src.snake import *
 from src.food import Food
 
@@ -20,6 +24,11 @@ class Game:
         self.snake = Snake(BLOCK_SIZE, BOUNDS)
         self.food = Food(BLOCK_SIZE, BOUNDS, self.snake)
 
+        # check (and create) the 'records' csv file
+        path = os.path.join(os.getcwd(), 'Data')
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
     def print_score(self, window, pos_x, pos_y):
         """
         This method is to  be called in the later method 'draw'. This one takes
@@ -29,6 +38,38 @@ class Game:
             f'Score: {self.score}', True, (255, 97, 3))
 
         window.blit(score_render, (pos_x, pos_y))
+
+    def test_save(self):
+        """
+        TO REMOVE!!
+        """
+        test_dics = [{
+            'user': 'Ferran',
+            'record': 1000,
+            'date': date.today()
+        },
+            {
+                'user': 'Jans',
+                'record': 800,
+                'date': date.today()
+        },
+            {
+                'user': 'Alpha',
+                'record': 300,
+                'date': date.today()
+        }]
+
+        with open('test_records.pck', 'wb') as handle:
+            pickle.dump(test_dics, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def read_records(self):
+        """
+        Method to read historic records of the game from disk.
+        """
+        with open('test_records.pck', 'rb') as handle:
+            test_read_dics = pickle.load(handle)
+
+        print(test_read_dics)
 
     def events(self, game):
         """Returns True if the game need to be stopped"""
@@ -97,6 +138,8 @@ def main():
         game.evolve(pygame)
 
         game.draw(pygame, window)
+
+    game.read_records()
 
     pygame.quit()
 
