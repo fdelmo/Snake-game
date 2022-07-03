256dba28894176d9942c1a5b909a331a0c4c8667
Ferran <ferrandelmoral@gmail.com>
2022-07-03 18:31:02 +0200

    Prototype of welcome screen

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,4 +1,6 @@
+from pickle import FRAME
 import tkinter as tk
+from turtle import down
 from typing import *
 
 
@@ -12,19 +14,34 @@ class GUI:
         # MAY NOT WORK, TRY WRITING SEPARATED OR * NOTATION
         self.window.config(padx=pad[0], pady=[1])
         self.window.eval('tk::PlaceWindow . center')
+        self.welcome_text = "This is a prototype to test the GUI of SnAIke. \n Enjoy the ride!"
 
     def WelcomeScreen(self) -> None:
         """
         Welcome screen with welcome message, play button and learn more button.
         """
-        self.window.geometry('675x400')
+        self.window.geometry('675x400')  # TODO: decide on final size
+
+        frame = tk.Frame(self.window)
+        frame.config(pady=150)
+        frame.pack(side=tk.TOP)
+
+        lbl = tk.Label(frame, text=self.welcome_text)
+        lbl.config(padx=2, pady=10, justify=tk.CENTER)
+        lbl.pack(side=tk.TOP)
+
+        button = tk.Button(frame, text="Start", command=self.InitialMenu)
+        button.config(relief=tk.RAISED, padx=8, pady=4)
+        button.pack(side=tk.BOTTOM)
 
     def InitialMenu(self) -> None:
         pass
 
 
 if __name__ == '__main__':
-    gui = GUI("test", (15, 10))
+    gui = GUI("SnAIke Prototype", (15, 10))
 
     gui.WelcomeScreen()
+
+    # main loop
     gui.window.mainloop()
