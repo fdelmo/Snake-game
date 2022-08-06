4e2d811d3be35d9b40d731856baf817a2f4156e8
Ferran <ferrandelmoral@gmail.com>
2022-08-06 12:28:42 +0200

    Changes in main

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from gui.gui import GUI
 
-if __name__ == '__main__':
+
+def main() -> None:
+    """
+    Main function of the program.
+    """
+
     gui = GUI("SnAIke Prototype", (15, 10))
 
     gui.WelcomeScreen()
 
     gui.window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
